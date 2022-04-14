1db7845bfd33ffa7592c1e125f728efebcae954b
Matt Ruder <matt.j.ruder@gmail.com>
2022-04-14 10:43:43 -0600

    Add metronome

--- main.js
+++ main.js
@@ -296,6 +296,35 @@ const minimumBpmArea = document.querySelector('.bpm-range-min-input-section')
 const maximumBpmArea = document.querySelector('.bpm-range-max-input-section')
 const bpmSelections = document.querySelector('.bpm-selections')
 
+//Metronome
+var metronome = new Metronome();
+var tempo = document.getElementById('tempo');
+tempo.textContent = metronome.tempo;
+
+var playPauseIcon = document.getElementById('play-pause-icon');
+
+var playButton = document.getElementById('play-button');
+playButton.addEventListener('click', function() {
+    metronome.startStop();
+
+    if (metronome.isRunning) {
+        playPauseIcon.className = 'pause';
+    }
+    else {
+        playPauseIcon.className = 'play';
+    }
+
+});
+
+var tempoChangeButtons = document.getElementsByClassName('tempo-change');
+for (var i = 0; i < tempoChangeButtons.length; i++) {
+    tempoChangeButtons[i].addEventListener('click', function() {
+        metronome.tempo += parseInt(this.dataset.change);
+        tempo.textContent = metronome.tempo;
+    });
+}
+//Metronome
+
 
 practiceMelodic.addEventListener('click', displayMelodicDropdowns)
 practiceRhythmic.addEventListener('click', displayRhythmicDropdowns)
